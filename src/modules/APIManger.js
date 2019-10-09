568e9870033027e11137d7ad6010080a2100fbcb
Gradi Ellis <[ellis.gradi@gmail.com]>
2019-10-09 16:17:49 -0500

    adding users

---
+++ src/modules/APIManger.js
@@ -0,0 +1,38 @@
+/* You're going to eliminate the possibility of duplicate code by making a module
+whose sole responsibility is to interact with the API.*/
+
+const remoteURL = "http://localhost:5002"
+
+export default {
+    get(resource, id) {
+      return fetch(`${remoteURL}/${resource}/${id}`).then(result => result.json())
+    },
+    getAll(resource) {
+      return fetch(`${remoteURL}/${resource}`).then(result => result.json())
+    },
+
+    delete(resource ,id) {
+      return fetch(`http://localhost:5002/${resource}/${id}`, {
+        method: "DELETE"
+    })
+      .then(result => result.json())
+  },
+  post(resource, newResource) {
+    return fetch(`${remoteURL}/${resource}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newResource)
+    }).then(data => data.json())
+  },
+  update(resource,editedResource) {
+    return fetch(`${remoteURL}/${resource}/${editedResource.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedResource)
+    }).then(data => data.json());
+  }
+  }
