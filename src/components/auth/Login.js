212144aa453611ab410a4c4ab1e00dce6ec58753
QuinWithOneN <jquinsmyth@gmail.com>
2019-10-14 09:06:06 -0500

    name shows up with chat

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -12,6 +12,7 @@ class Login extends Component {
     // Set initial state
 
     state = {
+        name: "",
         email: "",
         password: "",
         userId: "",
@@ -43,13 +44,16 @@ class Login extends Component {
                 window.alert("Please enter email")
             } else if (this.state.password === "") {
                 window.alert("Please enter password")
+            } else if (this.state.name === "") {
+                    window.alert("Please enter your name")
             } else if (singleUser) {
                 sessionStorage.setItem("userId", singleUser.id);
                 sessionStorage.setItem("email", this.state.email);
+                sessionStorage.setItem("name", this.state.name);
                this.props.triggerRender();
                 this.props.history.push("/");
             } else {
-            window.alert("User email and password do not match")
+            window.alert("Credentials do not match")
         }
 
     })
@@ -72,6 +76,11 @@ render() {
                     <form onSubmit={this.handleLogin}>
                         <fieldset>
                             <div className="formgrid">
+                            <label htmlFor="inputEmail">Name</label><br></br>
+                                <input onChange={this.handleFieldChange} type="text"
+                                    id="name"
+                                    placeholder="Name"
+                                    required="" autoFocus="" /><br></br>
                             <label htmlFor="inputEmail">Email address</label><br></br>
                                 <input onChange={this.handleFieldChange} type="email"
                                     id="email"
